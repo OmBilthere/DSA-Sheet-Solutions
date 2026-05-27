fbb25f655d3a3eae705afcdfe904ea6adac499ca
Om Bilthere <ombilthere953@gmail.com>
2026-05-27 21:52:43 +0530

    Check Anagram

---
+++ String/ChekAnagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        int n = s.size() , m = t.size();
+
+        if( n != m ) return false;
+        
+        unordered_map<int , int>mpp;
+
+        for(int i=0 ; i<n ; i++) {
+
+            mpp[s[i]]++;
+
+            mpp[t[i]]--;
+        }
+           
+        
+
+        for(auto it: mpp) 
+
+        if(it.second != 0 ) return false;
+    
+        return true;
+        
+    }
+};
